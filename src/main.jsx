9d569a9005be63a14d1d47d7846537773d688408
BinduVarsha23 <binduvarshasunkara@gmail.com>
2025-05-27 14:53:56 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import './styles/Search.css';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
